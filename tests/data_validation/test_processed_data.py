1f444f5cb25f2efea8571f87daac4b637e8a9dbb
SergeyZinchenko <zinchse@Air-13-main.local>
2024-04-29 14:44:05 +0700

    delete data and change tests

--- tests/data_validation/test_processed_data.py
+++ tests/data_validation/test_processed_data.py
@@ -19,30 +19,24 @@ def test_existence(bench_name: str):
 
 
 @pytest.mark.parametrize("bench_name", EXPECTED_BENCHS)
-def test_sizes(bench_name: str):
+def test_stucture(bench_name: str):
     path_to_bench = f"{PATH_TO_DATASET}/{bench_name}"
 
-    expected_size = BENCH_NAME_TO_EXPECTED_SIZE[bench_name]
+    expected_bench_size = BENCH_NAME_TO_EXPECTED_SIZE[bench_name]
     found_sqls = [f for f in os.listdir(path_to_bench) if f.endswith(".json")]
-    assert len(found_sqls) == expected_size, f"Found {len(found_sqls)} queries, but expected {expected_size}"
+    assert len(found_sqls) == expected_bench_size, f"Found {len(found_sqls)} queries, but expected {expected_size}"
 
     for query_file in os.listdir(path_to_bench):
         if not query_file.endswith(".json"):
             continue
         with open(f"{path_to_bench}/{query_file}", "r") as query_data_file:
             expected_query_data_size = EXPECTED_QUERY_DATA_SIZE
-            real_query_data_size = len(json.load(query_data_file))
+            query_data = json.load(query_data_file)
+            real_query_data_size = len(query_data)
+
             assert (
                 real_query_data_size == expected_query_data_size
             ), f"Data size for {query_file} is {real_query_data_size} but expected {expected_query_data_size}"
 
-
-@pytest.mark.parametrize("bench_name", EXPECTED_BENCHS)
-def test_plans(bench_name: str):
-    path_to_bench = f"{PATH_TO_DATASET}/{bench_name}"
-    for query_file in os.listdir(path_to_bench):
-        if not query_file.endswith(".json"):
-            continue
-        with open(f"{path_to_bench}/{query_file}", "r") as query_data_file:
-            for settings_str, plans in json.load(query_data_file).items():
+            for settings_str, plans in query_data.items():
                 assert Plans(**plans), f"Problem plans with {settings_str} for {query_file}"
