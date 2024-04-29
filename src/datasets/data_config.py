5bd0c944449afcaac615ca7a61640311b873276b
SergeyZinchenko <zinchse@Air-13-main.local>
2024-04-29 14:53:33 +0700

    fixed formatting

--- src/datasets/data_config.py
+++ src/datasets/data_config.py
@@ -27,4 +27,8 @@ DEFAULT_HINTSET: "HintsetCode" = 0
 DOPS: "List[QueryDop]" = [1, 16, 64]
 HINTSETS: "List[HintsetCode]" = list(range(2 ** len(HINTS)))
 BENCH_NAMES: "List[str]" = ["JOB", "sample_queries", "tpch_10gb"]
-BENCH_NAME_TO_SIZE: "Dict[str, int]" = {"JOB": 113, "sample_queries": 40, "tpch_10gb": 22}
+BENCH_NAME_TO_SIZE: "Dict[str, int]" = {
+    "JOB": 113,
+    "sample_queries": 40,
+    "tpch_10gb": 22,
+}
