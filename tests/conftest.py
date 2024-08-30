fc82843576de36d4a776bd1f8cf4151a80c589f3
SergeyZinchenko <zinchse@MacBook-Air.local>
2024-08-30 18:36:06 +0700

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+ROOT_PATH = os.path.abspath(os.getcwd())
+print(ROOT_PATH)
+HBO_BENCH_PATH = os.path.join(ROOT_PATH, "hbo_bench/src/hbo_bench")
+sys.path.insert(0, ROOT_PATH)
+
+from typing import Dict
+import pytest
+from hbo_bench.oracle import Oracle
+from wrappers import ORACLES_DICT, initialize_oracles
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_oracles():
+    initialize_oracles(root_path=HBO_BENCH_PATH, bench_names=["JOB", "sample_queries"])
+
+
+@pytest.fixture
+def oracles_dict() -> "Dict[str, Oracle]":
+    return ORACLES_DICT
---
+++ tests/test_storage.py
@@ -0,0 +1,69 @@
+from storage import Storage, Node, Transition, Parameter, DISTANCE_THRESHOLD
+import pytest
+
+EPS = 1e-10
+
+
+@pytest.fixture
+def node_from() -> "Node":
+    return Node(logical_plan="<plan_from1>", template_id=1, selectivities=[0.1], cardinalities=[1])
+
+
+@pytest.fixture
+def node_to() -> "Node":
+    return Node(logical_plan="<plan_to1>", template_id=1, selectivities=[0.5], cardinalities=[2])
+
+
+@pytest.fixture
+def another_node_to() -> "Node":
+    return Node(logical_plan="<plan_to2>", template_id=1, selectivities=[0.5], cardinalities=[2])
+
+
+@pytest.fixture
+def node_close_to_node_to() -> "Node":
+    return Node(
+        logical_plan="<plan_to1>",
+        template_id=1,
+        selectivities=[0.5 * (DISTANCE_THRESHOLD - EPS)],
+        cardinalities=[2 * (DISTANCE_THRESHOLD - EPS)],
+    )
+
+
+@pytest.fixture
+def unknown_node() -> "Node":
+    return Node(logical_plan="<unknown_plan>", template_id=3, selectivities=[0.42], cardinalities=[42])
+
+
+def test_default_scenario(node_from: "Node", node_to: "Node"):
+    st = Storage()
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=0.5, parameter=Parameter(1, 666))
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=100, parameter=Parameter(2, 777))
+    parameters = st.get_promised_parameters(node_from.template_id)
+    assert parameters == {Parameter(1, 666), Parameter(2, 777)}
+
+
+def test_collision_scenario(node_from: "Node", node_to: "Node", another_node_to: "Node"):
+    st = Storage()
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=0.5, parameter=Parameter(1, 666))
+    st.add_info(Transition(node_from=node_from, node_to=another_node_to), boost=100, parameter=Parameter(2, 777))
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=1000, parameter=Parameter(1, 6666))
+    parameters = st.get_promised_parameters(node_from.template_id)
+    assert parameters == {Parameter(1, 666), Parameter(1, 6666), Parameter(2, 777)}
+    assert st.estimate_transition(Transition(node_from=node_from, node_to=node_to)) == (0.5 + 1000) / 2
+
+
+def test_generalisation(node_from: "Node", node_to: "Node", node_close_to_node_to: "Node"):
+    st = Storage()
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=0.5, parameter=Parameter(1, 666))
+    assert st.estimate_transition(Transition(node_from=node_from, node_to=node_close_to_node_to)) == 0.5
+
+
+def test_unknown_node_scenario(node_from: "Node", node_to: "Node", unknown_node: "Node"):
+    st = Storage()
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=0.5, parameter=Parameter(1, 666))
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=100, parameter=Parameter(2, 777))
+    st.add_info(Transition(node_from=node_from, node_to=node_to), boost=1000, parameter=Parameter(1, 6666))
+    parameters = st.get_promised_parameters(unknown_node.template_id)
+    assert parameters == set()
+    assert st.estimate_transition(Transition(node_from=unknown_node, node_to=node_to)) == float("-inf")
+    assert st.estimate_transition(Transition(node_from=unknown_node, node_to=unknown_node)) == 0.0
